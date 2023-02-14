c97d389b02c208ad90ac77a462722e49f7767496
davdemete <davdemeter@gmail.com>
2023-02-14 13:17:57 +0100

    pruebas de java

---
+++ PRUEBAS/main.js
@@ -0,0 +1,13 @@
+const saludos = (persona: string) => {
+    return "Hola, " + persona;
+    216
+    }
+    const usuario: string = "Marcos";
+    console.log(usuario);
+    document.body.innerHTML = saludos(usuario);
+    var saludos = function (persona) {
+        return "Hola, " + persona;
+        };
+        var usuario = "Marcos";
+        console.log(usuario);
+        document.body.innerHTML = saludos(usuario);
